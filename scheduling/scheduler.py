2f4e7f2a64a46a2e9858549027f72da7d28fd077
diffraction-zebra <pankov.va@phystech.edu>
2023-04-07 22:19:36 +0300

    add main structure

---
+++ scheduling/scheduler.py
@@ -0,0 +1,19 @@
+import kivy
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+
+
+class Task:
+    pass
+
+
+class Plan:
+    pass
+
+
+class Scheduler:
+    pass
+
+
+class Constructor:
+    pass
